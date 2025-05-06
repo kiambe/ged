a710b8e4121b70024788581ddf70408839f30a52
kiambe <kevinkiambe@gmail.com>
2025-05-06 12:56:08 +0300

    Git to SQLite

--- ged_backend/ged_backend/settings.py
+++ ged_backend/ged_backend/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'ged_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ged_db2',
-        'USER': 'ged_user2',
-        'PASSWORD': 'G34iN47iCA@2063',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ged_db2',
+#         'USER': 'ged_user2',
+#         'PASSWORD': 'G34iN47iCA@2063',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
